8d0956026157e71d696387f543e46c1adc3b5fb7
= <=>
2019-05-31 13:16:06 -0300

    Revertendo Ajustes

--- src/main/java/br/com/b2w/service/PlanetaRestService.java
+++ src/main/java/br/com/b2w/service/PlanetaRestService.java
@@ -26,6 +26,12 @@ public class PlanetaRestService {
 	private HttpEntity<String> entity;
 	
 	public PlanetaRestService() {
+		      //Erro 403
+				restTemplate = new RestTemplate();
+				HttpHeaders headers = new HttpHeaders();
+		        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+		        entity = new HttpEntity<String>("parameters", headers);
 		
 	}
 	
